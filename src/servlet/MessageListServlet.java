8bfe844f5a9d426125c35f51dffa3f9b674af606
NikitaDunskiy <nikita.dunskiy@gmail.com>
2020-06-01 14:28:29 +0300

    completed MessageListServlet

--- src/servlet/MessageListServlet.java
+++ src/servlet/MessageListServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,17 +13,15 @@ import entity.ChatMessage;
 public class MessageListServlet extends ChatServlet {
     private static final long serialVersionUID = 1L;
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Установить кодировку HTTP-ответа UTF-8
         response.setCharacterEncoding("utf8");
-        // Получить доступ к потоку вывода HTTP-ответа
         PrintWriter pw = response.getWriter();
-        // Записть в поток HTML-разметку страницы
-        pw.println("<html><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8'/><meta http-equiv='refresh' content='1'></head>");
+        pw.println("<html><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8'/><meta http-equiv='refresh' content='10'></head>");
         pw.println("<body>");
-        // В обратном порядке записать в поток HTML-разметку для каждого сообщения
         for (int i=messages.size()-1; i>=0; i--) {
             ChatMessage aMessage = messages.get(i);
-            pw.println("<div><strong>" + aMessage.getAuthor().getName() + "</strong>: " + aMessage.getMessage() + "</div>");
+            SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+            Date date = new Date(aMessage.getTimestamp());
+            pw.println("<div><strong>" + aMessage.getAuthor().getName() + "(" + sdf.format(date) + ")"+"</strong>: " + aMessage.getMessage() + "</div>");
         }
         pw.println("</body></html>");
     }
